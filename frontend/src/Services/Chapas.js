e2f0ad7828d28463f10a070af88747b311d6169b
Jean Ferri <ferri@avioeste.local>
2024-08-29 10:12:51 -0300

    Feito alterações iniciais e criado services para Usuarios e Chapas

---
+++ frontend/src/Services/Chapas.js
@@ -0,0 +1,138 @@
+const token = process.env.REACT_APP_TOKEN_API
+
+
+async function pegarTodasChapas(){
+    try{
+        const options = {
+            method: 'GET',
+            headers : {
+                'Content-Type': 'application/json',
+                "authorization": `Bearer ${token}`
+            }
+        }
+        const result = await fetch(`${process.env.REACT_APP_URL_BACKEND}/chapas`,options)
+        const data = await result.json()
+        return data
+    }catch(err){
+        console.error('Erro ao pegar chapas' , err)
+        throw err
+    }}
+
+async function pegarChapaPorId(id){
+    try{
+        const options = {
+            method : 'GET',
+            headers : {
+                'Content-Type': 'application/json',
+                "authorization": `Bearer ${token}`
+            }
+        }
+        const result = await fetch(`${process.env.REACT_APP_URL_BACKEND}/chapas/${id}`,options)
+        const data = await result.json()
+        return data
+    }catch(err){
+        console.error('Erro ao pegar chapa pelo ID')
+        throw err
+    }
+}
+
+async function pegarChapaPorCodigo(codigo){
+    try{
+        const options = {
+            method: 'GET',
+            headers : {
+                'Content-Type': 'application/json',
+                "authorization": `Bearer ${token}`
+            }
+        }
+        const result = await fetch(`${process.env.REACT_APP_URL_BACKEND}/chapas/codigo/${codigo}`,options)
+        const data = await result.json()
+        return data
+    }catch(err){
+        console.error('Erro ao pegar chapa por codigo')
+        throw err
+    }
+}
+
+async function pegarChapaPorMaterial(id){
+    try{
+        const options = {
+            method: 'GET',
+            headers : {
+                'Content-Type': 'application/json',
+                "authorization": `Bearer ${token}`
+            }
+        }
+        const result = await fetch(`${process.env.REACT_APP_URL_BACKEND}/chapas/material/${id}`,options)
+        const data = await result.json()
+        return data
+    }catch(err){
+        console.error('Erro ao pegar chapa por material')
+        throw err
+    }
+}
+
+async function cadastrarChapa(codigo,nome,comprimento,altura,espessura,mat_id){
+    try {
+        const options = {
+            method: 'POST',
+            headers : {
+                'Content-Type': 'application/json',
+                "authorization": `Bearer ${token}`
+            },
+            body : JSON.stringify({
+                cha_codigo: codigo,
+                cha_nome: nome,
+                cha_comprimento: comprimento,
+                cha_altura: altura,
+                cha_espessura: espessura,
+                mat_id: mat_id
+            })
+        }
+        const result  = await fetch(`${process.env.REACT_APP_URL_BACKEND}/chapas/create`,options)
+        const data = await result.json()
+        return data
+    }catch(err){
+        console.error('Erro ao cadastrar chapa')
+        throw err
+    }
+}
+
+async function deletarChapaPorId(id){
+    try{
+        const options = {
+            method: 'DELETE',
+            headers : {
+                'Content-Type': 'application/json',
+                "authorization": `Bearer ${token}`
+            }
+        }
+        const result = await fetch(`${process.env.REACT_APP_URL_BACKEND}/chapas/delete/${id}`,options) 
+        const data = await result.json()
+        return data
+    }catch(err){
+        console.error('Erro ao deletar chapa')
+        throw err
+    }
+}
+
+async function pegarChapasParaCalculo(mat_id,cha_espessura){
+    try{
+        const options = {
+            method: 'POST',
+            headers : {
+                'Content-Type': 'application/json',
+                "authorization": `Bearer ${token}`
+            },
+            body : JSON.stringify({
+                mat_id: mat_id,
+                cha_espessura: cha_espessura
+            })
+        }
+    }catch(err){
+        console.error('Erro ao pegar chapas para calculo')
+        throw err
+    }
+}
+
+    export {pegarTodasChapas , pegarChapaPorId , pegarChapaPorCodigo , pegarChapaPorMaterial, cadastrarChapa , deletarChapaPorId, pegarChapasParaCalculo}
---
+++ frontend/src/Services/Usuarios.js
@@ -0,0 +1,133 @@
+const token = process.env.REACT_APP_TOKEN_API
+
+async function PegaTodosOsUsuarios(){
+    try{
+        const options = {
+            method: 'GET',
+            headers : {
+                'Content-Type': 'application/json',
+                "authorization": `Bearer ${token}`
+            }
+        }
+        const result = await fetch(`${process.env.REACT_APP_URL_BACKEND}/usuarios`,options)
+        const data = await result.json()
+        return data
+    }catch(err){
+        console.error('Erro ao pegar usuarios' , err)
+        throw err
+    }
+    }
+
+async function PegaUsuarioPorId(id){
+    try{
+        const options = {
+            method: 'GET',
+            headers : {
+                'Content-Type': 'application/json',
+                "authorization": `Bearer ${token}`
+            }
+        }
+        const result = await fetch(`${process.env.REACT_APP_URL_BACKEND}/usuarios/${id}`,options)
+        const data = await result.json()
+        return data
+    }catch(err){
+        console.error('Erro ao pegar usuario por id' , err)
+        throw err
+    }
+}
+
+async function criarUsuario(nome,email,senha){
+    try{
+        const options = {
+            method:'POST',
+            headers: 
+                {
+                    'Content-Type': 'application/json',
+                    "authorization": `Bearer ${token}`
+                },
+            body : JSON.stringify({
+                usu_nome: nome,
+                usu_email: email,
+                usu_senha: senha
+            })
+    }
+    const result = await fetch(`${process.env.REACT_APP_URL_BACKEND}/usuarios/create`,options)
+    const data = await result.json()
+    return data
+
+}catch(err){
+    console.error('Erro ao criar usuario' , err)
+    throw err
+}
+}
+
+async function deletarUsuarioPorId(id){
+    try{
+        const options = {
+            method:'DELETE',
+            headers: 
+                {
+                    'Content-Type': 'application/json',
+                    "authorization": `Bearer ${token}`
+                }
+            }
+            const result = await fetch(`${process.env.REACT_APP_URL_BACKEND}/usuarios/delete/${id}`,options)
+            const data = await result.json()
+            return data
+    }catch(err){
+        console.error('Erro ao deletar usuario' , err)
+        throw err
+    }
+}
+
+async function alterarSenhaUsuario(id,senha){
+    try{
+        const options = {
+            method : 'PATCH',
+            headers: 
+                {
+                    'Content-Type': 'application/json',
+                    "authorization": `Bearer ${token}`
+                },
+            body : JSON.stringify({
+                usu_id: id,
+                usu_senha: senha
+            })
+        }
+        const result = await fetch(`${process.env.REACT_APP_URL_BACKEND}/usuarios/update/senha`,options)
+        const data = await result.json()
+        return data
+    }catch(err){
+        console.error('Erro ao alterar senha' , err)
+        throw err
+    }
+}
+
+async function loginUsuario(email,senha){
+    try{
+        const options = {
+            method : 'POST',
+            headers: 
+            {
+                'Content-Type': 'application/json',
+                "authorization": `Bearer ${token}`
+            },
+            body : JSON.stringify({
+                usu_email: email,
+                usu_senha: senha
+            })
+        }
+        const result = await fetch(`${process.env.REACT_APP_URL_BACKEND}/usuarios/login`,options)
+        const data = await result.json()
+        return data
+    }catch(err){
+        console.error('Erro ao login' , err)
+        throw err
+    }
+}
+
+
+
+
+    
+export  { PegaTodosOsUsuarios , PegaUsuarioPorId, criarUsuario, deletarUsuarioPorId ,alterarSenhaUsuario , loginUsuario }
